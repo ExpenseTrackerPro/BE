eeb06e0e3b4e68fb4762dc5d243003a32cc0e5e2
jenalan <thayalanjenalan18@gmail.com>
2026-06-12 10:39:03 +0530

    AuthResponseDto.java

---
+++ src/main/java/com/sgic/Expense/Tracker/dto/AuthResponseDto.java
@@ -0,0 +1,16 @@
+package com.sgic.Expense.Tracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthResponseDto {
+    private String token;
+    private String email;
+    private String name;
+}
