d29f81acb904e0abc1f90b35ba831d813ba1f7c0
jenalan <thayalanjenalan18@gmail.com>
2026-06-17 09:19:37 +0530

    CategoriesMapper.java

--- src/main/java/com/sgic/Expense/Tracker/mapper/CategoriesMapper.java
+++ src/main/java/com/sgic/Expense/Tracker/mapper/CategoriesMapper.java
@@ -1,5 +1,6 @@
 package com.sgic.Expense.Tracker.mapper;
 
+
 import com.sgic.Expense.Tracker.dto.CategoriesDto;
 import com.sgic.Expense.Tracker.entity.Categories;
 import org.mapstruct.Mapper;
@@ -8,9 +9,10 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CategoriesMapper {
-  CategoriesDto toDto(Categories category);
-  Categories toEntity(CategoriesDto dto);
-  List<Categories> toEntityList(List<CategoriesDto> categoriesDtos);
-  List<CategoriesDto> toDtoList(List<Categories> categories);
+CategoriesDto toDto(Categories categories);
+
+Categories toEntity(CategoriesDto categoriesDto);
+
+List<CategoriesDto> toDtoList(List<Categories> categoriesList);
 
 }
