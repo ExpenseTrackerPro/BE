fb6ac9c65fa7ab0fdc4b16e817c78aab3af948dc
jenalan <thayalanjenalan18@gmail.com>
2026-06-17 09:25:04 +0530

    UserMapper.java

--- src/main/java/com/sgic/Expense/Tracker/mapper/UserMapper.java
+++ src/main/java/com/sgic/Expense/Tracker/mapper/UserMapper.java
@@ -1,4 +1,15 @@
 package com.sgic.Expense.Tracker.mapper;
 
+import com.sgic.Expense.Tracker.dto.UserDto;
+import com.sgic.Expense.Tracker.entity.User;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface UserMapper {
+    List<UserDto> toDtoList (List<User> userList);
+    UserDto toDto(User user);
+    User toEntity(UserDto userDto);
+    List<User> toEntityList(List<UserDto> userDtos);
 }
