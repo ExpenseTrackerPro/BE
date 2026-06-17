34b1ade5757f10061f87008448eac2ec53dee9cf
jenalan <thayalanjenalan18@gmail.com>
2026-06-17 09:21:21 +0530

    GroupMapper.java

--- src/main/java/com/sgic/Expense/Tracker/mapper/GroupMapper.java
+++ src/main/java/com/sgic/Expense/Tracker/mapper/GroupMapper.java
@@ -1,4 +1,12 @@
 package com.sgic.Expense.Tracker.mapper;
 
-public class GroupMapper {
+import com.sgic.Expense.Tracker.dto.GroupDto;
+import com.sgic.Expense.Tracker.entity.Group;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface GroupMapper {
+    GroupDto toDto(Group group);
+    Group toEntity(GroupDto groupDto);
 }
