46400c6e607cafff1f27c3ba5f5ef339a8e0780c
jenalan <thayalanjenalan18@gmail.com>
2026-06-17 09:10:20 +0530

    CategoriesDto.java

--- src/main/java/com/sgic/Expense/Tracker/dto/CategoriesDto.java
+++ src/main/java/com/sgic/Expense/Tracker/dto/CategoriesDto.java
@@ -1,9 +1,16 @@
 package com.sgic.Expense.Tracker.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoriesDto {
 
     private Long id;
     private String name;
-    private String Description;
-    private String Status;
+    private String description;
+    private String status;
 }
