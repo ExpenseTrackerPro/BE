254b52e59b4a9bae173a648d7c45705fa834c685
jenalan <thayalanjenalan18@gmail.com>
2026-06-17 09:13:06 +0530

    Categories.java

--- src/main/java/com/sgic/Expense/Tracker/entity/Categories.java
+++ src/main/java/com/sgic/Expense/Tracker/entity/Categories.java
@@ -3,23 +3,30 @@ package com.sgic.Expense.Tracker.entity;
 import com.sgic.Expense.Tracker.utils.DateAudit;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
+@Table(name = "categories")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
+@EqualsAndHashCode(callSuper = false)
 public class Categories extends DateAudit {
     @Id
-    @GeneratedValue
-   private Long id;
-   private String name;
-   private String Description;
-   private String Status;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String status;
+
     @OneToMany(mappedBy = "categories")
-    List<Group>groupList;
+    private List<Group> groupList;
+
 
 }
