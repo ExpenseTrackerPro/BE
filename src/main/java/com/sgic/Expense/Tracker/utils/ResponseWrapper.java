6fe11b4337df7a5e6e25deb0c39039cb17be2f24
jenalan <thayalanjenalan18@gmail.com>
2026-06-17 09:33:12 +0530

    ResponseWrapper.java

--- src/main/java/com/sgic/Expense/Tracker/utils/ResponseWrapper.java
+++ src/main/java/com/sgic/Expense/Tracker/utils/ResponseWrapper.java
@@ -1,4 +1,17 @@
 package com.sgic.Expense.Tracker.utils;
 
-public class ResponseWrapper {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseWrapper<T> {
+    private int statusCode;
+    private String statusMessage;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
 }
